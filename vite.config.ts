582443be76d36fa277fda8bbbe9ae08e0e5e3538
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-10-16 18:53:20 -0600

    feat: se agrega configuracion para los paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {find : '@', replacement: path.resolve(__dirname, 'src')},
+      {find : '@assets', replacement: path.resolve(__dirname, 'src/assets')},
+      {find : '@components', replacement: path.resolve(__dirname, 'src/components')},
+      {find : '@icons', replacement: path.resolve(__dirname, 'src/icons')},
+      {find : '@data', replacement: path.resolve(__dirname, 'src/data')},
+      {find : '@helpers', replacement: path.resolve(__dirname, 'src/helpers')},
+      {find : '@store', replacement: path.resolve(__dirname, 'src/store')}
+    ]
+  }
 })
