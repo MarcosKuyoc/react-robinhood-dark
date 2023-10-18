41f4a8610ac5207f06b8a831d20f547c9dbc76d4
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-10-17 18:27:09 -0600

    feat: se agrega un nuevo servicio para hacer las peticiones a la API

--- src/services/historic-tickers.ts
+++ src/services/historic-tickers.ts
@@ -1,12 +1,19 @@
 import { dateFormat } from "@/helpers/parse-date";
 import { EodResponse } from "./tickers.interface";
 
-export const fetchHistoricTickers = async (symbol: string, startDate: string, endDate: string) => {
-  console.log(`${startDate}-${endDate}`);
-  const res = await fetch(`/data/eod-${symbol}.json`);
+export const fetchHistoricTickers = async (symbol: string, dateFront: string, dateTo: string) => {
+  console.log(`${symbol}-${dateFront}-${dateTo}`);
+  let name = 'eod-MSFT-1M.json';
+  if (dateFront === '2023-10-10') {
+    name = 'eod-MSFT-1W.json';
+  }
+  if (dateFront === '2023-04-20') {
+    name = 'eod-MSFT-6M.json';
+  }
+  const res = await fetch(`/data/${name}`);
   const data = await res.json();
   if (!res.ok) throw new Error('Network not found');
-  const eod: EodResponse[] = data;
+  const eod: EodResponse[] = data.data.eod;
 
   const result = eod.map((item) => {
     return  {
